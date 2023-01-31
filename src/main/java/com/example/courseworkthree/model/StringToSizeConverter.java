fca6d25331e0029be41ee67cad02311040bc10b4
LinarDaminov <linar.daminov@yandex.ru>
2023-01-31 13:35:03 +0500

    fifth commit

--- src/main/java/com/example/courseworkthree/model/StringToSizeConverter.java
+++ src/main/java/com/example/courseworkthree/model/StringToSizeConverter.java
@@ -1,10 +1,8 @@
 package com.example.courseworkthree.model;
 
 
-import com.example.courseworkthree.model.Size;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.databind.util.Converter;
+
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,16 +12,5 @@ public class StringToSizeConverter implements Converter<String, Size> {
     public Size convert(String source) {
         return Size.forValues(source);
     }
-
-    @Override
-    public JavaType getInputType(TypeFactory typeFactory) {
-        return null;
-    }
-
-    @Override
-    public JavaType getOutputType(TypeFactory typeFactory) {
-        return null;
-    }
-
 
 }
